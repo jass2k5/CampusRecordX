51c2b3c93d0f87e051b8a83a278dc7d7984d581a
jass2k5 <jsssndl@gmail.com>
2025-08-03 00:11:56 +0530

    Added get_user_input helper to support quitting with 'q' or 'e'

--- src/manager.py
+++ src/manager.py
@@ -1,13 +1,18 @@
 from utils.input_validators import (is_valid_email,
  is_valid_text_input, is_valid_roll,is_valid_phone,is_valid_cgpa)
+from utils.input_helper import get_user_input
 from student import Student
 
 class StudentManager:
     def __init__(self):
         self.students = {}
     def add_students(self):
+        print("enter q to exit other wise your name")
         while True:
-            name = input("please enter your name here:\n").title().strip()
+            name = get_user_input("please enter your name here:\n")
+            if name is None:
+                return
+            name = name.title().strip()
             if not is_valid_text_input(name):
                 print("⚠️ invalid input".title())
                 continue
@@ -17,7 +22,10 @@ class StudentManager:
                 break
 
         while True:
-                roll_input = input("enter you roll no.").title().strip()
+                roll_input = get_user_input("enter you roll no.")
+                if roll_input is None:
+                    return
+                roll_input = roll_input.strip()
                 roll = is_valid_roll(roll_input)
                 if roll is not None:
                     print(f"entered roll number:\n {roll}")
@@ -27,7 +35,10 @@ class StudentManager:
                     continue
               
         while True:
-            course = input("please enter your course:\n").title().strip()
+            course = get_user_input("please enter your course:\n")
+            if course is None:
+                return
+            course = course.title().strip()
             if not is_valid_text_input(course):
                 print("⚠️enter a valid course name ")
                 continue
@@ -35,7 +46,9 @@ class StudentManager:
                 print(f"entered course:\n{course}")
                 break
         while True:
-            phone = input("enter your mobile number")
+            phone = get_user_input("enter your mobile number")
+            if phone is None:
+                return
             if not is_valid_phone(phone):
                 print("⚠️enter a valid 10 digit number please")
                 continue
@@ -43,7 +56,9 @@ class StudentManager:
                 print(f"entered phone number is:\n{phone}")
                 break
         while True:
-            email = input("enter your email")
+            email = get_user_input("enter your email")
+            if email is None:
+                return
             if is_valid_email(email):
                 print("valid email ✅")
                 break
@@ -51,7 +66,9 @@ class StudentManager:
                 print("invalid email❌")
                 continue
         while True:
-          cgpa = input("enter your cgpa")
+          cgpa = get_user_input("enter your cgpa")
+          if cgpa is None:
+               return
           if not is_valid_cgpa:
               print("⚠️invalid input try again")
               continue
@@ -71,18 +88,33 @@ class StudentManager:
         else:
             for i,(roll,student) in enumerate(self.students.items(),start = 1):
                 print(f"{roll} -- {student}")
-            print("total students in the class:".title())
-            print(len(self.students))
             self.search_bycourse()
     def search_bycourse(self):
-        course_query = input("enter the name of your course").title().strip()
+        course_query = get_user_input("enter the name of your course").title().strip()
+        if course_query is None:
+            return
         found = [s for s in self.students.values() if s.course == course_query]  #the calling str will trigger __str__ method of object student or s 
         if found:
             studentswithcourse = list(map(lambda s: str(s),found))
             for student in studentswithcourse:
                 print(student)
-                
-   
+        else:
+            print("⚠️ no such user with this course found")
+        self.remove_student_by_roll()
+  
+    def remove_student_by_roll(self):
+        print("e or q to exit")
+        roll_2= get_user_input("enter your roll number")
+        roll = is_valid_roll(roll_2)
+        if roll is not None:
+            if roll in self.students:
+                del self.students[roll]
+            else:
+                print("student not found")
+        else:
+            ("⚠️ invalid roll number")
+        self.display_all()
+    
            
 btech_sec1= StudentManager()
 btech_sec1.add_students()
---
+++ src/utils/input_helper.py
@@ -0,0 +1,9 @@
+# utils/input_helpers.py
+def get_user_input(prompt):
+    user_input = input(prompt).strip()
+    
+    if user_input.lower() in ['q', 'e']:
+        print("Exiting input... 🏃‍♂️")
+        return None
+
+    return user_input
